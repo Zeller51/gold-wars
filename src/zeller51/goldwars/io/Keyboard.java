039f5409c1db5c29992859b297372263f2413bc0
Zeller51 <thezeller51@gmail.com>
2014-04-12 19:27:22 +1000

    Added new Keyboard Listener

--- src/zeller51/goldwars/io/Keyboard.java
+++ src/zeller51/goldwars/io/Keyboard.java
@@ -4,70 +4,65 @@ import java.awt.Component;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Keyboard implements KeyListener {
-
-	private List<Key> keys = new ArrayList<Key>();
-
-	public Key up = new Key(keys);
-	public Key down = new Key(keys);
-	public Key left = new Key(keys);
-	public Key right = new Key(keys);
 
 	public Keyboard(Component c) {
 		c.addKeyListener(this);
 	}
 
-	public void update() {
-		for (int i = 0; i < keys.size(); i++) {
-			keys.get(i).update();
-		}
-	}
+	private ArrayList<String> keys = new ArrayList<String>();
+	private ArrayList<Boolean> clicked = new ArrayList<Boolean>();
 
-	private void toggle(boolean state, int keyCode) {
-		if (keyCode == KeyEvent.VK_W)
-			up.toggle(state);
-		if (keyCode == KeyEvent.VK_S)
-			down.toggle(state);
-		if (keyCode == KeyEvent.VK_A)
-			left.toggle(state);
-		if (keyCode == KeyEvent.VK_D)
-			right.toggle(state);
+	public void tick() {
+		for (int i = 0; i < clicked.size(); i++) {
+			if (clicked.get(i).booleanValue() == true) {
+				clicked.remove(i);
+				clicked.add(i, new Boolean(false));
+			}
+		}
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		toggle(true, e.getKeyCode());
+		boolean dupe = false;
+		for (int i = 0; i < keys.size(); i++) {
+			if (keys.get(i).contentEquals(KeyEvent.getKeyText(e.getKeyCode()))) {
+				dupe = true;
+			}
+		}
+		if (!dupe) {
+			keys.add(KeyEvent.getKeyText(e.getKeyCode()));
+			clicked.add(new Boolean(true));
+		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		toggle(false, e.getKeyCode());
+		clicked.remove(keys.indexOf(KeyEvent.getKeyText(e.getKeyCode())));
+		keys.remove(KeyEvent.getKeyText(e.getKeyCode()));
+
+	}
+
+	public boolean down(String key) {
+		for (int i = 0; i < keys.size(); i++) {
+			if (key.contentEquals(keys.get(i))) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean clicked(String key) {
+		for (int i = 0; i < keys.size(); i++) {
+			if (key.contentEquals(keys.get(i))) {
+				return clicked.get(i).booleanValue();
+			}
+		}
+		return false;
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 	}
-
-	private class Key {
-		public boolean down = false;
-		public boolean clicked = false;
-
-		public Key(List<Key> keys) {
-			keys.add(this);
-		}
-
-		public void toggle(boolean state) {
-			down = state;
-			clicked = state;
-		}
-
-		public void update() {
-			if (down && clicked) {
-				clicked = false;
-			}
-		}
-	}
-
 }
